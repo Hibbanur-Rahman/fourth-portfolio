3d792319295a88bfff2f8dabbf59af95c662341d
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-10-01 17:20:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,58 @@
 //portfolio navbar 
-var navItems=document.querySelectorAll('.portfolioNavitem');
-var itemListing=document.querySelector('.item_listing');
-var brand=document.querySelectorAll('.brand');
-var design=document.querySelectorAll('.design');
-var photos=document.querySelectorAll('.photos');
+var navItems = document.querySelectorAll('.portfolioNavitem');
+var itemListing = document.querySelector('.item_listing');
+var brand = document.querySelectorAll('.brand');
+var design = document.querySelectorAll('.design');
+var photos = document.querySelectorAll('.photos');
 
-navItems.forEach((element)=>{
-    element.addEventListener('click',()=>{
-        if(element.id=='all'){
-            console.log(itemListing.firstElementChild);
-            var itemListingRow=itemListing.firstElementChild;
-            console.log(itemListingRow);
-            itemListingRow.innerHTML='';
-            itemListing.innerHTML='';
+navItems.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (element.id == 'all') {
+            var itemListingRow = itemListing.firstElementChild;
+            itemListingRow.innerHTML = '';
+            itemListing.innerHTML = '';
             itemListing.appendChild(itemListingRow);
-            brand.forEach((element1)=>{
+            brand.forEach((element1) => {
                 itemListingRow.appendChild(element1);
             })
-            design.forEach((element1)=>{
-                itemListingRow.appendChild(element1);
+           var row=document.createElement('div');
+           row.className='row';
+           itemListing.appendChild(row);
+            design.forEach((element1) => {
+                row.appendChild(element1);
             })
-            photos.forEach((element1)=>{
+            photos.forEach((element1) => {
+                row.appendChild(element1);
+                // itemListingRow.appendChild(element1);
+            })
+        }
+        else if (element.id == 'brand') {
+            var itemListingRow = itemListing.firstElementChild;
+            itemListingRow.innerHTML = '';
+            
+            itemListing.innerHTML = '';
+            itemListing.appendChild(itemListingRow);
+            brand.forEach((element1) => {
                 itemListingRow.appendChild(element1);
             })
         }
-        else if(element.id=='brand'){
-            console.log(itemListing.firstElementChild);
-            var itemListingRow=itemListing.firstElementChild;
-            console.log(itemListingRow);
-            itemListingRow.innerHTML='';
-            itemListing.innerHTML='';
+        else if (element.id == 'design') {
+            var itemListingRow = itemListing.firstElementChild;
+            itemListingRow.innerHTML = '';
+            itemListing.innerHTML = '';
             itemListing.appendChild(itemListingRow);
-            brand.forEach((element1)=>{
+            design.forEach((element1) => {
                 itemListingRow.appendChild(element1);
             })
         }
-        else if(element.id=='design'){
+        else if (element.id == 'photos') {
             console.log(itemListing.firstElementChild);
-            var itemListingRow=itemListing.firstElementChild;
+            var itemListingRow = itemListing.firstElementChild;
             console.log(itemListingRow);
-            itemListingRow.innerHTML='';
-            itemListing.innerHTML='';
+            itemListingRow.innerHTML = '';
+            itemListing.innerHTML = '';
             itemListing.appendChild(itemListingRow);
-            design.forEach((element1)=>{
-                itemListingRow.appendChild(element1);
-            })
-        }
-        else if(element.id=='photos'){
-            console.log(itemListing.firstElementChild);
-            var itemListingRow=itemListing.firstElementChild;
-            console.log(itemListingRow);
-            itemListingRow.innerHTML='';
-            itemListing.innerHTML='';
-            itemListing.appendChild(itemListingRow);
-            photos.forEach((element1)=>{
+            photos.forEach((element1) => {
                 itemListingRow.appendChild(element1);
             })
         }
