ed898394a3a56754c536a98e3a2638e6414a82dd
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-10-11 14:13:00 +0530

    change in testimonial

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var brand = document.querySelectorAll('.brand');
 var design = document.querySelectorAll('.design');
 var photos = document.querySelectorAll('.photos');
 
+
 navItems.forEach((element) => {
     element.addEventListener('click', () => {
         if (element.id == 'all') {
@@ -48,13 +49,37 @@ navItems.forEach((element) => {
         else if (element.id == 'photos') {
             console.log(itemListing.firstElementChild);
             var itemListingRow = itemListing.firstElementChild;
-            console.log(itemListingRow);
             itemListingRow.innerHTML = '';
             itemListing.innerHTML = '';
             itemListing.appendChild(itemListingRow);
             photos.forEach((element1) => {
                 itemListingRow.appendChild(element1);
             })
+        }
+    })
+})
+
+
+//testimonial slider
+var testimonialSlider=document.querySelector('.slider');
+var Control1=document.querySelector('.control-1');
+var Control2=document.querySelector('.control-2');
+var controlArr=document.querySelectorAll('.control');
+
+Control1.style.backgroundColor='#d1cfcf';
+controlArr.forEach((element)=>{
+    element.addEventListener('click',()=>{
+        if(element.classList.contains('control-1')){
+            testimonialSlider.style.transform='translateX(25%)';
+            testimonialSlider.style.transition='all 0.5s ease-in-out';
+            Control1.style.backgroundColor='#d1cfcf';
+            Control2.style.backgroundColor='#e4e4e4';
+        }
+        else if(element.classList.contains('control-2')){
+            testimonialSlider.style.transform='translateX(-25%)';
+            testimonialSlider.style.transition='all 0.5s ease-in-out';
+            Control1.style.backgroundColor='#e4e4e4';
+            Control2.style.backgroundColor='#d1cfcf';
         }
     })
 })
